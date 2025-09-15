d672f72ef9596d17a506cf0f1b516708da67513d
BenHadman <benhadman@gmail.com>
2025-09-15 09:36:49 +0100

    Update layout.tsx

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -2,17 +2,14 @@ import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
 
-const domine = Domine({ subsets: ["latin"], variable: "--font-sans" })
-const mona  = Mona_Sans({ subsets: ["latin"], variable: "--font-serif" })
-
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-mode="light" className={`${inter.variable} ${lora.variable}`}>
-      <body className="font-sans">
+    <html lang="en" data-mode="light">
+      <body>
         <main className="relative">{props.children}</main>
       </body>
     </html>
