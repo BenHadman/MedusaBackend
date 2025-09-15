46523453b07f00f229a94108d5aa166627249cee
BenHadman <benhadman@gmail.com>
2025-09-15 10:36:35 +0100

    Added fonts over google cdn

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -9,6 +9,15 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+      <head>
+        {/* Google Fonts CDN */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&family=Mona+Sans:wdth,wght@75..125,200..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <main className="relative">{props.children}</main>
       </body>
