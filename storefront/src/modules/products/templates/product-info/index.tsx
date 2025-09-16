0e1705adcb7e7d29331fd3f4762faf02e03b60b3
BenHadman <benhadman@gmail.com>
2025-09-16 17:37:46 +0100

    Update index.tsx - products - product-info

--- storefront/src/modules/products/templates/product-info/index.tsx
+++ storefront/src/modules/products/templates/product-info/index.tsx
@@ -8,26 +8,26 @@ type ProductInfoProps = {
 
 const ProductInfo = ({ product }: ProductInfoProps) => {
   return (
-    <div id="product-info">
+    <div id="product-info" className="text-text-200">
       <div className="flex flex-col gap-y-4 lg:max-w-[500px] mx-auto">
         {product.collection && (
           <LocalizedClientLink
             href={`/collections/${product.collection.handle}`}
-            className="text-medium text-ui-fg-muted hover:text-ui-fg-subtle"
+            className="text-medium text-text-300 hover:text-accent-main-200 underline underline-offset-4 decoration-border-300"
           >
             {product.collection.title}
           </LocalizedClientLink>
         )}
         <Heading
           level="h2"
-          className="text-3xl leading-10 text-ui-fg-base"
+          className="font-serif text-3xl leading-10 text-text-100"
           data-testid="product-title"
         >
           {product.title}
         </Heading>
 
         <Text
-          className="text-medium text-ui-fg-subtle whitespace-pre-line"
+          className="text-medium text-text-300 whitespace-pre-line"
           data-testid="product-description"
         >
           {product.description}
