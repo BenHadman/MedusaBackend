1b8de75f60395e1dab9673257bd87e3854ff88d9
BenHadman <benhadman@gmail.com>
2025-09-17 20:47:19 +0100

    image-gallery

--- storefront/src/modules/products/components/image-gallery/index.tsx
+++ storefront/src/modules/products/components/image-gallery/index.tsx
@@ -1,41 +1,26 @@
-import { HttpTypes } from "@medusajs/types"
-import { Container } from "@medusajs/ui"
 import Image from "next/image"
 
-type ImageGalleryProps = {
-  images: HttpTypes.StoreProductImage[]
-}
-
-const ImageGallery = ({ images }: ImageGalleryProps) => {
+export default function ImageGallery({ images = [] }) {
   return (
-    <div className="flex items-start relative">
-      <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
-        {images.map((image, index) => {
-          return (
-            <Container
-              key={image.id}
-              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
-              id={image.id}
-            >
-              {!!image.url && (
-                <Image
-                  src={image.url}
-                  priority={index <= 2 ? true : false}
-                  className="absolute inset-0 rounded-rounded"
-                  alt={`Product image ${index + 1}`}
-                  fill
-                  sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
-              )}
-            </Container>
-          )
-        })}
+    <div className="w-full relative bg-bg-100 border border-border-200 rounded-large overflow-hidden">
+      {/* main image */}
+      {/* … your existing logic */}
+      <div className="flex gap-2 p-2">
+        {images.map((img) => (
+          <button
+            key={img.id}
+            className="rounded-base border border-border-200 bg-bg-000 overflow-hidden"
+          >
+            <Image
+              src={img.url}
+              alt="Thumbnail"
+              width={72}
+              height={96}
+              className="object-cover"
+            />
+          </button>
+        ))}
       </div>
     </div>
   )
 }
-
-export default ImageGallery
