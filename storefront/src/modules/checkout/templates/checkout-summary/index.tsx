a8f169841168974296371de1de380d267ab3450e
BenHadman <benhadman@gmail.com>
2025-09-17 20:06:21 +0100

    checkout summary index.tsx

--- storefront/src/modules/checkout/templates/checkout-summary/index.tsx
+++ storefront/src/modules/checkout/templates/checkout-summary/index.tsx
@@ -7,16 +7,16 @@ import Divider from "@modules/common/components/divider"
 
 const CheckoutSummary = ({ cart }: { cart: any }) => {
   return (
-    <div className="sticky top-0 flex flex-col-reverse small:flex-col gap-y-8 py-8 small:py-0 ">
-      <div className="w-full bg-white flex flex-col">
-        <Divider className="my-6 small:hidden" />
+    <div className="sticky top-0 flex flex-col-reverse small:flex-col gap-y-8 py-8 small:py-0">
+      <div className="w-full bg-bg-100 border border-border-200 rounded-large p-6">
+        <Divider className="my-6 small:hidden border-border-200" />
         <Heading
           level="h2"
-          className="flex flex-row text-3xl-regular items-baseline"
+          className="flex flex-row font-serif text-3xl text-text-100 items-baseline"
         >
           In your Cart
         </Heading>
-        <Divider className="my-6" />
+        <Divider className="my-6 border-border-200" />
         <CartTotals totals={cart} />
         <ItemsPreviewTemplate items={cart?.items} />
         <div className="my-6">
