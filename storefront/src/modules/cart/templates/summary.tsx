4eab3b401d616cee5d991ef2a188f63b802dbe4f
BenHadman <benhadman@gmail.com>
2025-09-17 20:02:20 +0100

    Update cart - templates - summary.tsx

--- storefront/src/modules/cart/templates/summary.tsx
+++ storefront/src/modules/cart/templates/summary.tsx
@@ -28,18 +28,20 @@ const Summary = ({ cart }: SummaryProps) => {
   const step = getCheckoutStep(cart)
 
   return (
-    <div className="flex flex-col gap-y-4">
-      <Heading level="h2" className="text-[2rem] leading-[2.75rem]">
+    <div className="flex flex-col gap-y-4 text-text-200">
+      <Heading level="h2" className="font-serif text-[2rem] leading-[2.75rem] text-text-100">
         Summary
       </Heading>
       <DiscountCode cart={cart} />
-      <Divider />
+      <Divider className="border-border-200" />
       <CartTotals totals={cart} />
       <LocalizedClientLink
         href={"/checkout?step=" + step}
         data-testid="checkout-button"
       >
-        <Button className="w-full h-10">Go to checkout</Button>
+        <Button className="w-full h-11 rounded-large tracking-[0.08em] uppercase bg-accent-main-200 border border-accent-main-200 text-oncolor-100 hover:bg-accent-main-100 transition-bg">
+          Go to checkout
+        </Button>
       </LocalizedClientLink>
     </div>
   )
