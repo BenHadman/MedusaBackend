559b55066950c555bea44eeaef828b94f35e58ae
BenHadman <benhadman@gmail.com>
2025-09-17 19:56:55 +0100

    Update modeules- cart - tempalte - items.tsx

--- storefront/src/modules/cart/templates/items.tsx
+++ storefront/src/modules/cart/templates/items.tsx
@@ -13,11 +13,13 @@ const ItemsTemplate = ({ items }: ItemsTemplateProps) => {
   return (
     <div>
       <div className="pb-3 flex items-center">
-        <Heading className="text-[2rem] leading-[2.75rem]">Cart</Heading>
+        <Heading className="font-serif text-[2rem] leading-[2.75rem] text-text-100 tracking-[0.005em]">
+          Cart
+        </Heading>
       </div>
       <Table>
         <Table.Header className="border-t-0">
-          <Table.Row className="text-ui-fg-subtle txt-medium-plus">
+          <Table.Row className="text-text-300 txt-medium-plus border-b border-border-200">
             <Table.HeaderCell className="!pl-0">Item</Table.HeaderCell>
             <Table.HeaderCell></Table.HeaderCell>
             <Table.HeaderCell>Quantity</Table.HeaderCell>
